36a797c67cb46f6966e907da359b915b48d5fcea
kenta asakura <asakura.kenta963420@gmail.com>
2023-05-19 22:23:08 +0900

    footer image changed

--- dist/index.e5ac7667.js
+++ dist/index.e5ac7667.js
@@ -582,6 +582,86 @@ togglerMenuLinks.forEach((link)=>{
         togglerMenu.classList.remove("show");
     });
 });
+for(let i = 1; i <= 5; i++){
+    if (i === 3) continue;
+    console.log(i);
+}
+// OUTPUT: 1 2 4 5
+while(condition);
+do ;
+while (condition);
+switch(x){
+    case option1:
+        break;
+    case option2:
+        break;
+    default:
+}
+function limesToCut(wedgesNeeded, limes) {
+    let wedges = 0;
+    let limesCut = 0;
+    // iterates limes array
+    for(let i = 0; i < limes.length; i++){
+        // cuts current lime to specific wedges amount per size
+        switch(limes[i]){
+            case "small":
+                wedges += 6;
+                limesCut++;
+                break;
+            case "medium":
+                wedges += 8;
+                limesCut++;
+                break;
+            case "large":
+                wedges += 10;
+                limesCut++;
+                break;
+        }
+        // checks the current wedges count
+        if (wedgesNeeded === 0) return 0;
+        else if (wedges >= wedgesNeeded) break;
+    }
+    return limesCut;
+}
+// do {
+//   orders.shift();
+// } while (timeLeft > 0) {
+//   if (orders.length === 0) {
+//     orders = [];
+//   } else {
+//     orders.shift();
+//   }
+//   return orders;
+// }
+function timeToMixJuice(name) {
+    switch(name){
+        case "Pure Strawberry Joy":
+            return 0.5;
+        case "Energizer":
+        case "Green Garden":
+            return 1.5;
+        case "Tropical Island":
+            return 3;
+        case "All or Nothing":
+            return 5;
+        default:
+            return 2.5;
+    }
+}
+function remainingOrders(timeLeft, orders) {
+    do orders.shift();
+    while (timeLeft > 0);
+    if (orders.length === 0) return orders = [];
+    else for(let i = 0; i < orders.length; i++){
+        timeToMixJuice(orders[i]);
+        orders.shift();
+    }
+    return orders;
+}
+console.log(remainingOrders(5, [
+    "Energizer",
+    "Tropical Island"
+]));
 
 },{}]},["4PWRH","aitAD"], "aitAD", "parcelRequire5914")
 
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -30,3 +30,112 @@ togglerMenuLinks.forEach(link => {
 });
 
 
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    continue;
+  }
+  console.log(i);
+}
+// OUTPUT: 1 2 4 5
+
+while (condition) {
+  // code block
+}
+
+
+do {
+  // code block
+} while (condition);
+
+switch (x) {
+  case option1:
+    // code that is executed when "x === option1" is true
+    break;
+  case option2:
+    // code that is executed when "x === option2" is true
+    break;
+  default:
+    // code that is executed when x does not equal any of the options
+}
+
+function limesToCut(wedgesNeeded, limes) {
+  let wedges = 0;
+  let limesCut = 0;
+
+  // iterates limes array
+  for (let i = 0; i < limes.length; i++) {
+    // cuts current lime to specific wedges amount per size
+    switch (limes[i]) {
+      case 'small':
+        wedges += 6;
+        limesCut++;
+        break;
+      case "medium":
+        wedges += 8;
+        limesCut++;
+        break;
+      case "large":
+        wedges += 10;
+        limesCut++;
+        break;
+    }
+
+    // checks the current wedges count
+    if (wedgesNeeded === 0) {
+      return 0;
+    } else if (wedges >= wedgesNeeded) {
+      break;
+    }
+  }
+
+  return limesCut;
+}
+
+// do {
+//   orders.shift();
+// } while (timeLeft > 0) {
+//   if (orders.length === 0) {
+//     orders = [];
+//   } else {
+//     orders.shift();
+//   }
+//   return orders;
+  // }
+
+function timeToMixJuice(name) {
+  switch (name) {
+    case 'Pure Strawberry Joy':
+      return 0.5;
+      break;
+    case 'Energizer':
+    case 'Green Garden':
+      return 1.5
+      break;
+    case 'Tropical Island':
+      return 3;
+      break;
+    case 'All or Nothing':
+      return 5;
+      break;
+    default:
+      return 2.5;
+  }
+}
+
+function remainingOrders(timeLeft, orders) {
+  do {
+    orders.shift();
+  } while (timeLeft > 0) {
+    if (orders.length === 0) {
+      return orders = [];
+    } else {
+      for (let i = 0; i < orders.length; i++) {
+        timeLeft - timeToMixJuice(orders[i]);
+        orders.shift();
+      }
+    }
+  }
+  return orders;
+}
+
+console.log(remainingOrders(5, ['Energizer', 'Tropical Island']));
